26851fd3bf43cb054adeba2c7f91531f2985fe7c
Claire Miller <clairemiller@outlook.com>
2022-08-25 23:46:00 -0700

    inital commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)   
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
