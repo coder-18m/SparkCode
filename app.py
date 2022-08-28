e5e371493ea948c7f2e1e3fab1167a7dbe58c0d5
Claire Miller <clairemiller@outlook.com>
2022-08-28 11:20:36 -0700

    added logic to contact form

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 from flask import Flask, render_template, url_for
+from flask import request
+from flask_mail import Mail, Message
 
-app = Flask(__name__)   
+
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'millercm108@gmail.com'
+app.config['MAIL_PASSWORD'] = 'uhhszflqrmspunzk'
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+# mail = Mail()
+
+# mail.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        msg = Message(f"SparkCode Website: Message from {name}", sender=email, recipients=['millercm108@gmail.com'])
+        msg.body = message
+        mail.send(msg)
+
+        return render_template('contact.html', success=True)
+
     return render_template('contact.html')
-
-
 
 
 
